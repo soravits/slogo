d7b40beca27297c968087c766dabc1a761603002
Diane Hadley <dfh12@duke.edu>
2016-10-14 03:26:09 -0400

    Complete all view internal APIs for plan

---
+++ src/HelpWindow.java
@@ -0,0 +1,22 @@
+import javafx.scene.Group;
+
+/**
+ * The purpose of this class is to create the root that visualizes 
+ * how the user gets to the help window.
+ * This root can then be passed to UI to be displayed in the scene
+ * 
+ * @author Diane Hadley
+ */
+
+public interface HelpWindow{
+	
+	
+	/*
+	 * returns root with any UI for getting to help window from UI
+	 * 
+	 */	
+	public Group getRoot();
+	
+
+	
+}
