00c2165230bf1008a7c95895d2f0b89b3f8b2c98
Brian Keohane <bk112@duke.edu>
2016-10-23 21:57:59 -0400

    Updated Turtle Movement Commands

--- src/model/commands/Forward.java
+++ src/model/commands/Forward.java
@@ -17,11 +17,18 @@ public class Forward extends OneInputCommand{
         TurtleState turtle = this.getModel().getTurtle();
         Position prevPosition = turtle.getPosition().clone();
         LineState lines = this.getModel().getLineState();
-        double moveX = Math.sin(Math.toRadians(turtle.getTurtleAngle()))*this.getParam1();
-        double moveY = Math.cos(Math.toRadians(turtle.getTurtleAngle()))*this.getParam1();
-        turtle.updateTurtlePosition(moveX, moveY);
+        double[] coords = calculateCoordinates(turtle);
+        turtle.updateTurtlePosition(coords[0], coords[1]);
         lines.createLine(prevPosition, turtle.getPosition());
         return this.getParam1();
     }
+    
+    public double[] calculateCoordinates(TurtleState turtle){
+        double[] coords = new double[2];
+        coords[0] = Math.sin(Math.toRadians(turtle.getTurtleAngle()))*this.getParam1();
+        coords[1] = Math.cos(Math.toRadians(turtle.getTurtleAngle()))*this.getParam1();
+        return coords;
+    }
+    
 
 }
