110cd77949b6b8ec4ff7f11df4f35b82d91c3802
Diane Hadley <dfh12@duke.edu>
2016-10-24 01:54:07 -0400

    Add to help page and add change listener to language combo box (no listener yet)

--- src/view/GeneralSettings.java
+++ src/view/GeneralSettings.java
@@ -1,9 +1,13 @@
 package view;
 
+import controller.Controller;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.control.ComboBox;
+import javafx.scene.paint.Color;
 
 /**
  * 
@@ -19,14 +23,15 @@ public class GeneralSettings extends UIBuilder{
 	
 	private Group root = new Group();
 	private ComboBox<String> languageComboBox;
+	private Controller controller;
 	
 	private static int LANGUAGE_COMBOBOX_X = 140;
 	private static int LANGUAGE_COMBOBOX_Y = 10;
 	
 	
-	public GeneralSettings(){
+	public GeneralSettings(Controller controller){
 		super();
-		
+		this.controller = controller;
 	}
 	
 
@@ -59,6 +64,13 @@ public class GeneralSettings extends UIBuilder{
 			
 		languageComboBox = new ComboBox<String>(languageOptions);
 		languageComboBox.setValue(uiResources.getString("English"));
+		
+		languageComboBox.valueProperty().addListener(new ChangeListener<String>() {
+			@Override public void changed(ObservableValue color, String s1, String s2) {
+				
+			}
+		});	
+		
 		root.getChildren().add(setControlLayout(languageComboBox, LANGUAGE_COMBOBOX_X, LANGUAGE_COMBOBOX_Y, "generalcontrol"));
 	}
 	
