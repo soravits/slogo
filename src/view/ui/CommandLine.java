f559539c97312b72eb31f0d8d6cdb9f96f72ad26
Soravit Sophastienphong <ss833@duke.edu>
2016-10-30 19:20:21 -0400

    Added error support

--- src/view/ui/CommandLine.java
+++ src/view/ui/CommandLine.java
@@ -16,7 +16,7 @@ import view.DisplayError;
  */
 
 public class CommandLine implements UIAttributes{
-	
+
 	private Group root = new Group();
 	private TextArea textArea = new TextArea();
 	private DisplayError displayError = new DisplayError();
@@ -25,37 +25,36 @@ public class CommandLine implements UIAttributes{
 	private int commandLineHeight;
 	private int commandLineWidth;
 	private int commandLineY;
-	
+
 	private static int FIRST_BUTTON_X = 190;
 	private static int BUTTON_SPACING = 170;
 	private static int COMMAND_HISTORY_WINDOW_X = 300;
 	private static int COMMAND_HISTORY_WINDOW_Y = 300;
-	private int buttonsY; 
-	
-	
+	private int buttonsY;
+
+
 	private static int COMMAND_LINE_X = 10;
 	private UI ui;
-	
+
 	public CommandLine(int sceneHeight, UI ui){
 		super();
 		this.commandLineHeight = sceneHeight - TURTLE_CANVAS_HEIGHT - 120;
-		this.commandLineY = TURTLE_CANVAS_HEIGHT + 110; 
+		this.commandLineY = TURTLE_CANVAS_HEIGHT + 110;
 		this.commandLineWidth = COMMAND_LINE_WIDTH;
 		this.buttonsY = TURTLE_CANVAS_HEIGHT + 65;
 		this.ui = ui;
 		makeCommandLine();
 		getButtons();
-		root.getChildren().add(uiBuilder.getText(COMMAND_LINE_X, commandLineY - 10, 
+		root.getChildren().add(uiBuilder.getText(COMMAND_LINE_X, commandLineY - 10,
 				uiResources.getString("CommandLine")));
 	}
-	
-	
+
 	/*
 	 * returns root with all visualization of commandLine
 	 * 
-	 */	
+	 */
 	public Group getRoot(){
-		
+
 		return root;
 	}
 	
@@ -67,28 +66,27 @@ public class CommandLine implements UIAttributes{
 	public String getCommand(){
 		return textArea.getText();
 	}
-	
+
 	private void makeCommandLine(){
-		
+
 		textArea.setLayoutY(commandLineY);
 		textArea.setLayoutX(COMMAND_LINE_X);
 		textArea.setPrefWidth(commandLineWidth);
 		textArea.setPrefHeight(commandLineHeight);
 		root.getChildren().add(textArea);
 	}
-	
+
 	private void getButtons(){
-		
-		
-		Button history = uiBuilder.makeButton(FIRST_BUTTON_X + BUTTON_SPACING, buttonsY, 
+
+		Button history = uiBuilder.makeButton(FIRST_BUTTON_X + BUTTON_SPACING, buttonsY,
 				uiResources.getString("History"), "generalcontrol");
 		history.setOnAction((event) -> {
-				CommandHistoryWindow commandHistoryWindow = new CommandHistoryWindow(ui,this,COMMAND_HISTORY_WINDOW_X,COMMAND_HISTORY_WINDOW_Y);
-				commandHistoryWindow.updateCommandHistory(ui.getModel().getCommandHistory());
-				commandHistoryWindow.show();
-		});	
-		
-		Button submit = uiBuilder.makeButton(FIRST_BUTTON_X + BUTTON_SPACING*2, buttonsY, 
+			CommandHistoryWindow commandHistoryWindow = new CommandHistoryWindow(ui,this,COMMAND_HISTORY_WINDOW_X,COMMAND_HISTORY_WINDOW_Y);
+			commandHistoryWindow.updateCommandHistory(ui.getModel().getCommandHistory());
+			commandHistoryWindow.show();
+		});
+
+		Button submit = uiBuilder.makeButton(FIRST_BUTTON_X + BUTTON_SPACING*2, buttonsY,
 				uiResources.getString("Submit"), "generalcontrol");
 		submit.setOnAction((event) -> {
 			try {
@@ -97,11 +95,11 @@ public class CommandLine implements UIAttributes{
 				displayError.displayErrorDialogueBox(e.getMessage());
 			}
 			textArea.setText("");
-		});	
-		
+		});
+
 		root.getChildren().addAll(history, submit);
 	}
-	
+
 	public void setCommand(String command) {
 		textArea.setText(command);
 	}
